0fcfc48cafd6d5faebfd134bb66147242a5c3998
Aidahar <zagirik@gmail.com>
2021-10-16 20:44:33 +0000

    update

--- memory.go
+++ memory.go
@@ -1,18 +1,22 @@
 package memory
 
 type Memory struct {
-	Key   string
-	Value interface{}
+	cache map[string]interface{}
+}
+
+func New() *Memory {
+	return &Memory{
+		cache: make(map[string]interface{}),
+	}
 }
 
 func (m *Memory) Set(key string, value interface{}) {
-	m.Key = key
-	m.Value = value
+	m.cache[key] = value
 }
 
 func (m *Memory) Get(key string) interface{} {
-	return m.Value
+	return m.cache[key]
 }
 func (m *Memory) Delete(key string) {
-	m.Key = ""
+	delete(m.cache, key)
 }
