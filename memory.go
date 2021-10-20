1ddc74861e5e94803e828febef68610ad1e8b856
Aidahar <zagirik@gmail.com>
2021-10-20 23:03:04 +0300

    add new variant realisation func Get

--- memory.go
+++ memory.go
@@ -1,27 +1,37 @@
-package cache                                                                                                         
- 
+package cache
+
 type Cache struct {
-    cache map[string]interface{}
+	cache map[string]interface{}
 }
 
 func New() *Cache {
-   return &Cache{
-       cache: make(map[string]interface{}),
-   }
+	return &Cache{
+		cache: make(map[string]interface{}),
+	}
 }
 
 func (m *Cache) Set(key string, value interface{}) {
-    m.cache[key] = value
+	m.cache[key] = value
 }
 
 func (m *Cache) Get(key string) interface{} {
-    k, exists := m.cache[key]
-    if exists {
-        return k
-    }
-    return nil
+	k, exists := m.cache[key]
+	if exists {
+		return k
+	}
+	return nil
 
 }
+
+/*
+func (m *Cache) Get(key string) (interface{}, error) {
+	k, err := m.cache[key]
+	if !err {
+		return nil, errors.New("Такого ключа не существует!")
+	}
+	return k, nil
+}
+*/
 func (m *Cache) Delete(key string) {
-    delete(m.cache, key)
+	delete(m.cache, key)
 }
